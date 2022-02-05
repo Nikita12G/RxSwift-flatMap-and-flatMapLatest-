54652e0979736077a01f546793c454036f92cc7f
Nikita12G <gulyi08@mail.ru>
2022-02-05 17:44:08 +0700

    Initial Commit

---
+++ RxSwift(flatMap and flatMapLatest)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift(flatMap and flatMapLatest)
+//
+//  Created by Никита Гуляев on 05.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
